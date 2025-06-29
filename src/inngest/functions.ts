6048733e046e73ebf1107e37ec50c49dd0f5bee0
devdezzies <boltholt17@gmail.com>
2025-06-29 14:23:31 +0700

    06-e2b-sandbox: integrates e2b sandbox on inngest

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("neo-test-1"); 
+      return sandbox.sandboxId;
+    })
+
     const writer = createAgent({
       name: "writer",
       system: "You are an expert writer.  You write readable, concise, simple content.",
@@ -13,7 +20,12 @@ export const helloWorld = inngest.createFunction(
 
     const { output } = await writer.run(`Write a short story about this ${event.data.value}`)
 
-    await step.sleep("wait-a-moment", "2s");
-    return { message: output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId); 
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );
