8f696051ea76612911505027fbc14ba1f39088a5
devdezzies <boltholt17@gmail.com>
2025-06-30 14:29:22 +0700

    09: introduced new table and relations on schema and add project creation logics

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -17,12 +17,17 @@ export const messagesRouter = createTRPCRouter({
     create: baseProcedure
         .input(
             z.object({
-                value: z.string().min(1, { message: "Message is required" })
+                value: z.string()
+                    .min(1, { message: "Message is required" })
+                    .max(10000, { message: "Prompt is too long" }), 
+                projectId: z.string()
+                    .min(1, { message: "Project ID cannot be null"})
             }),
         )
         .mutation(async ({ input }) => {
             const createdMessage = await prisma.message.create({
                 data: {
+                    projectId: input.projectId,
                     content: input.value, 
                     role: "USER", 
                     type: "RESULT"
@@ -32,7 +37,8 @@ export const messagesRouter = createTRPCRouter({
             await inngest.send({
                 name: "code-agent/run", 
                 data: {
-                    value: input.value
+                    value: input.value, 
+                    projectId: input.projectId
                 }
             }); 
 
