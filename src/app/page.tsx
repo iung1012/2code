b9c974b268cf5aa974cd512c43dfe01b9557eae6
devdezzies <boltholt17@gmail.com>
2025-06-30 11:43:32 +0700

    08: add message schema + migration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,27 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
   const [value, setValue] = useState("");
   const trpc = useTRPC(); 
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const { data } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
-      toast.success("Background job started")
+      toast.success("Message created")
     }
   }));
 
   return (
     <div className="mx-w-7xl mx-auto p-4 flex items-center justify-center">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={invoke.isPending} className="cursor-pointer" onClick={() => invoke.mutate({ value: value })}>
+      <Button disabled={createMessage.isPending} className="cursor-pointer" onClick={() => createMessage.mutate({ value: value })}>
         Invoke function
       </Button>
+      {JSON.stringify(data)}
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,26 @@
 import { inngest } from "./client";
-import { openai, createAgent, createTool, createNetwork } from "@inngest/agent-kit";
+import { openai, createAgent, createTool, createNetwork, Tool } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import { z } from "zod";
 import { PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+interface AgentState {
+  summary: string, 
+  files: { [path: string]: string }
+}
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("neo-test-1"); 
       return sandbox.sandboxId;
     })
 
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description: "an expert AI coding agent",
       system: PROMPT,
@@ -66,11 +72,11 @@ export const helloWorld = inngest.createFunction(
               }),
             ),
           }), 
-          handler: async ({ files }, { step, network }) => {
+          handler: async ({ files }, { step, network }: Tool.Options<AgentState>) => {
             const newFiles = await step?.run("createOrUpdateFiles", async () => {
               try {
                 const updatedFiles = network.state.data.files || {}; 
-                const sandbox = getSandbox(sandboxId); 
+                const sandbox = await getSandbox(sandboxId); 
                 for (const file of files) {
                   (await sandbox).files.write(file.path, file.content); 
                   updatedFiles[file.path] = file.content;
@@ -124,7 +130,7 @@ export const helloWorld = inngest.createFunction(
       }
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name: "code-agent-network", 
       agents: [codeAgent], 
       maxIter: 15, 
@@ -138,10 +144,41 @@ export const helloWorld = inngest.createFunction(
 
     const result = await network.run(event.data.value);
 
+    const isError = 
+      !result.state.data.summary || 
+      Object.keys(result.state.data.files || {}).length === 0;
+
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId); 
-      const host =  sandbox.getHost(3000);
+      const host = sandbox.getHost(3000);
       return `https://${host}`;
+    })
+
+    await step.run("save-result", async () => {
+      if (isError) {
+        return await prisma.message.create({
+          data: {
+            content: "Something went wrong. Please try again", 
+            role: "ASSISTANT", 
+            type: "ERROR"
+          }
+        })
+      }
+
+      return await prisma.message.create({
+        data: {
+          content: result.state.data.summary, 
+          role: "ASSISTANT", 
+          type: "RESULT", 
+          fragment: {
+            create: {
+              sandboxUrl: sandboxUrl, 
+              title: "Fragment", 
+              files: result.state.data.files,
+            }
+          }
+        }
+      })
     })
 
     return {
